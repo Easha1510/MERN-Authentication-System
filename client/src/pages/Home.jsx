404a38d6551821dd68be41e712b6d39043e2b54f
Easha <easrni.slp@gmail.com>
2026-01-27 12:03:32 +0530

    update code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
 
 const Home = () => {
   return (
-    <div>Home page</div>
+    <div >
+        <Navbar/>
+        <Header/>
+    </div>
   )
 }
 
